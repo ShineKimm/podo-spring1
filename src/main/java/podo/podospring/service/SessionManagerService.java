196a67889b3b5275457727db549039e99c82af67
kdy <21ckdy@naver.com>
2022-12-20 16:48:52 +0900

    인터페이스 삭제

--- src/main/java/podo/podospring/service/SessionManagerService.java
+++ src/main/java/podo/podospring/service/SessionManagerService.java
@@ -2,8 +2,19 @@ package podo.podospring.service;
 
 import java.util.HashMap;
 import java.util.List;
+import org.springframework.stereotype.Service;
+import podo.podospring.dao.SessionManagerDAO;
 
-public interface SessionManagerService {
+@Service
+public class SessionManagerService {
 
-    List<HashMap<String, Object>> sessionConfirm(HashMap<String, Object> params);
+    SessionManagerDAO sessionManagerDAO;
+
+    public SessionManagerService(SessionManagerDAO sessionManagerDAO) {
+        this.sessionManagerDAO = sessionManagerDAO;
+    }
+
+    public List<HashMap<String, Object>> sessionConfirm(HashMap<String, Object> params) {
+        return sessionManagerDAO.sessionConfirm(params);
+    }
 }
